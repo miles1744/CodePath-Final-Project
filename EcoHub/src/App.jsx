ab967fb5e28bbfeae66f8dca634d70b908adeaa8
Miles1744 <mithomtab@gmail.com>
2025-04-21 15:48:01 -0400

    New

--- EcoHub/src/App.jsx
+++ EcoHub/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <button className="order-btn">Newest</button>
         <button className="order-btn">Most Popular</button>
       </div>
-    <div>
+    <div className="posts-container">
     {posts && posts.length > 0 ? posts.map((post) => (
       <div key={post.id}>
         <p>Posted {post.created_at}</p>
