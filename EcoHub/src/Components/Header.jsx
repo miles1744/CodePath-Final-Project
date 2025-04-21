ccd43299fb6fe70fb893d9a7f7c7f3cde1451f58
Miles1744 <mithomtab@gmail.com>
2025-04-21 12:56:22 -0400

    New

---
+++ EcoHub/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+const Header = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    return (
+        <>
+            <h1>EcoHub</h1>
+            <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        </>
+    )}
