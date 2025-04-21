8fe502f20551cfd5beb8bbf6373de8fa31ad2389
Miles1744 <mithomtab@gmail.com>
2025-04-21 16:32:24 -0400

    New

--- EcoHub/src/App.jsx
+++ EcoHub/src/App.jsx
@@ -6,6 +6,45 @@ import {supabase} from "./client.js"
 function App() {
   const [posts, setPosts] = useState([]);
 
+  const getFriendlyTimeAgo = (createdAt) => {
+    const { seconds, minutes, hours, days, months, years } = getTimeSincePost(createdAt);
+    if (seconds === 1) return "1 second ago";
+    if (seconds < 60 && seconds != 1) return `${seconds} seconds ago`;
+    if (minutes === 1) return "1 minute ago";
+    if (minutes < 60 && minutes != 1) return `${minutes} minutes ago`;
+    if (hours === 1) return "1 hour ago";
+    if (hours < 24 && hours != 1) return `${hours} hours ago`;
+    if (days === 1) return "1 day ago";
+    if (days < 30) return `${days} days ago`;
+    if (months === 1) return "1 month ago";
+    if (months < 12) return `${months} months ago`;
+    return `${years} years ago`;
+  };
+  
+
+  const getTimeSincePost = (createdAt) => {
+    const postDate = new Date(createdAt);
+    const now = new Date();
+    const diff = now - postDate; 
+  
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(diff / (1000 * 60));
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+    const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+  
+    return {
+      seconds,
+      minutes,
+      hours,
+      days,
+      months,
+      years
+    };
+  };
+  
+
   useEffect(() => {
       const fetchCrewmates = async () => {
           const {data} = await supabase
@@ -27,9 +66,10 @@ function App() {
       </div>
     <div className="posts-container">
     {posts && posts.length > 0 ? posts.map((post) => (
-      <div key={post.id}>
-        <p>Posted {post.created_at}</p>
-
+      <div className="post" key={post.id}>
+        <p>Posted {getFriendlyTimeAgo(post.created_at)}</p>
+        <h4>{post.Title}</h4>
+        <p>{post.Upvotes} upvotes</p>
       </div>
     )) :
 
