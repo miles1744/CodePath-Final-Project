bb3fffeb379039776eeade01952febe9a8d925b5
Miles1744 <mithomtab@gmail.com>
2025-04-21 15:22:32 -0400

    New

--- EcoHub/src/Components/CreateAPost.jsx
+++ EcoHub/src/Components/CreateAPost.jsx
@@ -19,9 +19,9 @@ const CreateAPost = () => {
         await supabase
             .from("Posts")
             .insert({
-                Title: {title},
-                Content: {content},
-                URL: {url},
+                Title: title,
+                Content: content,
+                URL: url,
                 Upvotes: 0
             })
             .select();
@@ -34,11 +34,11 @@ const CreateAPost = () => {
         <>
             <Outlet/>
             <div className="Create-Form-Container">
-                <form className="Create-Form">
+                <form className="Create-Form" onSubmit={createPost}>
                     <input className="input-bar" type="text" value={title} placeholder="Title" onChange={(e) => {setTitle(e.target.value)}}/>
                     <textarea className="text-area-content" value={content} placeholder="Content (Optional)" onChange={(e) => {setContent(e.target.value)}}> </textarea>
                     <input type="text" className="input-bar" value={url} placeholder="Image URL (Optional)" onChange={(e) => {setURL(e.target.value)}}/>
-                    <button className="post-btn"type="submit" onSubmit={createPost}>Create Post</button>
+                    <button className="post-btn" type="submit" >Create Post</button>
                 </form>
             </div>
         </>
