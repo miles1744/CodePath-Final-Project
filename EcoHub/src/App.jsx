2e8c5171c6aca078c10f58c8383fe51b0cfa93f9
Miles1744 <mithomtab@gmail.com>
2025-04-21 15:39:59 -0400

    New

--- EcoHub/src/App.jsx
+++ EcoHub/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import './Css/App.css'
 import {supabase} from "./client.js"
