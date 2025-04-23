41bea0311de2e6f27f2e9a308dbb0916bf8512e8
Miles1744 <mithomtab@gmail.com>
2025-04-23 17:48:33 -0400

    New

--- EcoHub/src/Components/PostPage.jsx
+++ EcoHub/src/Components/PostPage.jsx
@@ -8,7 +8,7 @@ const postPage = () =>{
 
     const {id} = useParams();
     const [post, setPost] = useState({});
-    const [comment, setComment] = useState("");
+    const [comment, setComment] = useState({});
     const [comments, setComments] = useState([]);
     const [upvotes, setUpvotes] = useState(0);
     const { session } = UserAuth();
@@ -26,21 +26,38 @@ const postPage = () =>{
         window.location = "/Home";
       }
 
-      const createPost = async (event) => {
+      const deleteComment = async (commentId) => {
+        event.preventDefault();
+      
+        await supabase
+          .from('Comments')
+          .delete()
+          .eq('id', commentId); 
+
+          setComments(comments.filter((c) => c.id !== commentId));
+
+        
+        window.location = `/Post/${id}`;
+      }
+
+      const createComment = async (event) => {
         event.preventDefault();
       
         const { data: { user } } = await supabase.auth.getUser();
       
          await supabase
-          .from('Posts')
+          .from('Comments')
           .insert({
-            Title: title,
-            post_id: content,
-            user_id: user.id,
+            comment: comment,
+            post_id: id,
+            user_id: session?.user?.id ,
           })
           .select();
-          
-        window.location.href = '/Home';
+
+         setComments([...comments, user["comment"]]); 
+         setComment(""); 
+         
+        window.location.href = `/Post/${id}`;
       };
       
     
@@ -146,16 +163,10 @@ const postPage = () =>{
       useEffect(() => {
         const fetchComments = async () => {
 
-          const { data: postData, error: fetchError } = await supabase
-          .from("Posts")
-          .select("Comments")
-          .eq("id", id)
-          .single();
-
           const { data, error } = await supabase
             .from("Comments")
             .select("*")
-            .eq("post_id", postData.id); 
+            .eq("post_id", id); 
           setComments(data);
         };
         fetchComments();
@@ -198,19 +209,15 @@ const postPage = () =>{
                         {
                             comments && comments.length > 0 ? comments.map((c, index) => (
                                 <div className="comment">
-                                  <p key={index}>- {c.text}</p>
-                                  
-                                  <div className="comment-section">
-                                    <svg onClick xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
-                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
-                                    </svg>
-
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                                    </svg>
-                                  </div>
-
+                                  <p key={index}>- {c.comment}</p>
+                                  {session?.user?.id === c.user_id && (
+                                    <div className="comment-section">
+                                      <svg onClick={() => deleteComment(c.id)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                      </svg>
+                                    </div>
+                                  )}
                                 </div>
                             )):
                             <div>
@@ -220,11 +227,11 @@ const postPage = () =>{
                             <input 
                             className="input-comment" 
                             placeholder="Leave a comment..." 
-                            type="text" value={comment} 
+                            type="text" value={comment.text} 
                             onChange={(e) => {setComment(e.target.value)}}
                             maxLength={145}
                             />
-                            <button className="input-btn" type="submit">Add Comment</button>
+                            <button onClick={createComment} className="input-btn" type="submit">Add Comment</button>
                         </form>
                     </div>
                     </div>
