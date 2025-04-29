f037a8c6fe1246bf762fa727a8ccf8b1377842a3
Miles1744 <mithomtab@gmail.com>
2025-04-29 17:29:05 -0400

    New

--- EcoHub/src/client.js
+++ EcoHub/src/client.js
@@ -1,6 +1,6 @@
 import {createClient} from "@supabase/supabase-js"
 
-const URL = "https://rcnshneadfvzybtnzcgz.supabase.co"
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjbnNobmVhZGZ2enlidG56Y2d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNDU2MjIsImV4cCI6MjA2MDgyMTYyMn0.OddYPaHy7PlZS8ij6HzBr3c7U1p7xLd7IzLUnnA3ReI"
+const URL = ""
+const API_KEY = ""
 
 export const supabase = createClient(URL, API_KEY)
